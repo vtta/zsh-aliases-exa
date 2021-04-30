874251ab8f2da8f487e3b3429f67372cd12f1fe6
vtta <41831480+vtta@users.noreply.github.com>
2021-04-30 15:44:41 +0800

    Update zsh-aliases-exa.plugin.zsh

--- zsh-aliases-exa.plugin.zsh
+++ zsh-aliases-exa.plugin.zsh
@@ -13,7 +13,7 @@ alias ls='exa'                                                         # ls
 alias l='exa -lbF --git'                                               # list, size, type, git
 alias ll='exa -lbGF --git'                                             # long list
 alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias la='exa -lbF --git'                                              # list, size, type, git + hidden
 alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
 
 # speciality views
